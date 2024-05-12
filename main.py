ca321c28b8fd61b41bf5662e2732e071ff790d6e
LingGuguang <1186986256@qq.com>
2024-05-12 21:46:44 +0800

    5/12

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ datasets = read_json("test.json")[:2]
 current_path = os.path.dirname(sys.path[0])
 model_path = os.path.join(current_path, "LLModel/qwen1.5-14B-chat")
 llm = QwenLLMChat(model_path)
-extractor = fewShotExtractor()
+extractor = fewShotExtractor(llm)
 QAlist = []
 for data in datasets:
     ids = data['ids']

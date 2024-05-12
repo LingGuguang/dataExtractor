71038079ef1398394718160cde465f123ea02fea
LingGuguang <1186986256@qq.com>
2024-05-12 21:19:53 +0800

    5/12

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 """
     利用大模型提取数据中的有效训练数据。
 """
-from utils.utilsForTemplate import TemplateExtractor
 from LLMs import QwenLLMChat
-from utils.utils import read_json
-
-from langchain.chains.llm import LLMChain
-from langchain_core.prompts import PromptTemplate
+from utils.basicUtils import read_json
 
 from dataExtractor import fewShotExtractor
 import os, sys
@@ -14,7 +10,7 @@ import os, sys
 datasets = read_json("test.json")[:2]
 current_path = os.path.dirname(sys.path[0])
 model_path = os.path.join(current_path, "LLModel/qwen1.5-14B-chat")
-llm = QwenLLMChat()
+llm = QwenLLMChat(model_path)
 extractor = fewShotExtractor()
 QAlist = []
 for data in datasets:
